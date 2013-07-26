6764e7b726db319c33d7ff8628e2b60be68115b5
Jan Buschtöns <buschtoens@gmail.com>
2013-07-26 08:07:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,154 @@
+/**
+ * Module dependencies.
+ */
+var EventEmitter = require("events").EventEmitter
+  , uuid = require("uuid");
+
+/**
+ * Expose `quevent`.
+ * @type Function
+ */
+module.exports = quevent;
+
+/**
+ * Monkey-patch the `events.EventEmitter`.
+ * @param  {events.EventEmitter} emitter
+ * @param  {String}              event   optional
+ * @return {events.EventEmitter|Function}
+ */
+function quevent(emitter, event) {
+  patch(emitter);
+  
+  if(event) { // event is specified, single mode
+    this.quevent.queue(event);
+    return this.quevent.emit.bind(this, event);
+  }
+
+  return emitter;
+}
+
+/**
+ * If not already patched, patch `emitter`.
+ * @param  {events.EventEmitter} emitter
+ */
+function patch(emitter) {
+  if(emitter.quevent) return;
+
+  emitter.quevent = { 
+      addListener: addListener.bind(emitter)
+    , _addListener: emitter.addListener.bind(emitter)
+    , emit: emit.bind(this)
+    , _emit: emitter.emit.bind(emitter)
+    , next: next.bind(this)
+    , queue: queue.bind(this)
+    , dequeue: dequeue.bind(this)
+    , queueing: queueing.bind(this)
+    , _queueing: []
+    , _cache: {}
+    };
+
+  emitter.on = emitter.addListener = emitter.quevent.addListener;
+  emitter.emit = emitter.quevent.emit;
+}
+
+/**
+ * Patched `addListener`.
+ * @param {String}   event
+ * @param {Function} listener
+ * @param {Boolean}  queue    optional
+ * @return {events.EventEmitter}
+ */
+function addListener(event, listener, queue) {
+  if(queue === false || !this.quevent.queueing(event))
+    return this.quevent._addListener(event, listener);
+
+  this.quevent.queue(event);
+  return this.quevent._addListener("quevent:" + event, listener);
+}
+
+/**
+ * Patched `emit`.
+ * @param  {String} event
+ * @return {events.EventEmitter}
+ */
+function emit(event) {
+  arguments = Array.prototype.slice.call(arguments, 0);
+
+  if(!this.quevent.queueing(event)) {
+    this.quevent._emit.apply(this, arguments);
+    arguments[0] = "quevent:" + event;
+    this.quevent._emit.apply(this, arguments);
+    return this;
+  }
+
+  var cache = this.cache[uuid.v4()] = {
+        arguments: arguments
+      , working: this.listeners(event).length
+      };
+  arguments.unshift(done.bind(this, cache));
+
+  this.quevent._emit.apply(this, arguments);
+
+  return this;
+}
+
+/**
+ * Called when a vip listener is done.
+ * @param  {Object} cache
+ */
+function done(cache) {
+  if(0 === --cache.working) {
+    arguments[0] = "quevent:" + arguments[0];
+    this.quevent._emit.apply(this, arguments);
+  }
+}
+
+/**
+ * Start queueing an `event` or turn it off.
+ * @param  {String} event
+ * @param  {Boolean} off  optional
+ * @return {events.EventEmitter}
+ */
+function queue(event, off) {
+  if(off === false)
+    return this.quevent.dequeue(event);
+
+  if(!~this.quevent._queueing.indexOf(event))
+    this.quevent._queueing.push(event);
+
+  return this;
+}
+
+/**
+ * Dequeue `event` and optionally emit it.
+ * @param  {String|Array} event
+ * @return {events.EventEmitter}
+ */
+function dequeue(event) {
+  if(arguments.length > 1)
+    this.quevent.emit.apply(this, arguments);
+  
+  if(event instanceof Array)
+    event.forEach(remove, this.quevent._queueing);
+  else if(typeof event === "string")
+    remove.call(this.quevent._queueing, event);
+
+  function remove(str) {
+    var i;
+    while((i = this.indexOf(str)) !== -1)
+      this.splice(i, 1);
+  }
+
+  return this;
+}
+
+/**
+ * Which events are queued.
+ * @param  {String} event
+ * @return {String|Array}
+ */
+function queueing(event) {
+  if(event)
+    return !!~this.quevent._queueing.indexOf(event);
+  return this.quevent._queueing.slice(0);
+}
