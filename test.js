48e92d0d1448178eb429560f0a1006992ccffef4
Jan Buschtöns <buschtoens@gmail.com>
2013-08-07 07:18:31 +0200

    Fix test.js

--- test.js
+++ test.js
@@ -6,25 +6,25 @@ var EventEmitter = require("events").EventEmitter
 
 var emitter = quevent(new EventEmitter).quevent.queue("test");
 
-emitter.on("test", function() {
+emitter.on("test", function(arg1, arg2, arg3) {
   console.log("NO VIP #1", arguments);
 });
-emitter.on("test", function() {
+emitter.on("test", function(arg1, arg2, arg3) {
   console.log("NO VIP #2", arguments);
 });
-emitter.on("test", function() {
+emitter.on("test", function(arg1, arg2, arg3) {
   console.log("NO VIP #3", arguments);
 });
 
-emitter.on("test", function(done) {
+emitter.on("test", function(arg1, arg2, arg3, done) {
   console.log("VIP #1", arguments);
   setTimeout(done, 500);
 }, false);
-emitter.on("test", function(done) {
+emitter.on("test", function(arg1, arg2, arg3, done) {
   console.log("VIP #2", arguments);
   setTimeout(done, 400);
 }, false);
-emitter.on("test", function(done) {
+emitter.on("test", function(arg1, arg2, arg3, done) {
   console.log("VIP #3", arguments);
   setTimeout(done, 300);
 }, false);
